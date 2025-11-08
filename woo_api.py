0fd549b37a0d096bc85ba9e6e0f9cfb3dae5d9d2
denmedia-dm <a.yazar@denmedia.nl>
2025-11-08 12:58:47 +0100

    Create woo_api.py

---
+++ woo_api.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+with open("config.json") as f:
+    CONFIG = json.load(f)
+
+WC_URL = CONFIG["woocommerce"]["url"]
+WC_KEY = CONFIG["woocommerce"]["consumer_key"]
+WC_SECRET = CONFIG["woocommerce"]["consumer_secret"]
+
+def get_woo_products():
+    """
+    WooCommerce'deki tüm ürünleri listeler
+    """
+    url = f"{WC_URL}/wp-json/wc/v3/products"
+    response = requests.get(url, auth=(WC_KEY, WC_SECRET))
+    return response.json()
+
+def update_woo_stock(product_id, new_stock):
+    """
+    WooCommerce'te stok günceller
+    """
+    url = f"{WC_URL}/wp-json/wc/v3/products/{product_id}"
+    data = {"stock_quantity": new_stock}
+    response = requests.put(url, auth=(WC_KEY, WC_SECRET), json=data)
+    return response.json()
