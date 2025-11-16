6592c904f910f4adfc0304460a3e86d381643a89
denmedia-dm <a.yazar@denmedia.nl>
2025-11-16 15:30:11 +0100

    Update woo_api.py

--- woo_api.py
+++ woo_api.py
@@ -13,28 +13,44 @@ WC_SECRET = CONFIG["woocommerce"]["consumer_secret"]
 # -------------------------------------------------------
 #   Tüm WooCommerce ürünlerini sınırsız şekilde çek
 # -------------------------------------------------------
+import requests
+import json
+
+with open("config.json") as f:
+    CONFIG = json.load(f)
+
+WC_URL = CONFIG["woocommerce"]["url"]
+WC_KEY = CONFIG["woocommerce"]["consumer_key"]
+WC_SECRET = CONFIG["woocommerce"]["consumer_secret"]
+
 def get_woo_products():
-    products = []
+    """
+    WooCommerce tüm ürünleri (sayfa sayfa) çeker.
+    """
+    all_products = []
     page = 1
+    per_page = 100  # en yüksek limit
 
     while True:
-        url = f"{WC_URL}/wp-json/wc/v3/products?per_page=100&page={page}"
-
+        url = f"{WC_URL}/wp-json/wc/v3/products?page={page}&per_page={per_page}"
         response = requests.get(url, auth=(WC_KEY, WC_SECRET))
 
-        try:
-            batch = response.json()
-        except Exception:
-            break
+        products = response.json()
 
-        # Eğer ürün yoksa dur
-        if not isinstance(batch, list) or len(batch) == 0:
-            break
+        if not products:
+            break  # ürün bitti
 
-        products.extend(batch)
+        all_products.extend(products)
+
+        # sayfa bilgisi bitti mi kontrol
+        if "X-WP-TotalPages" in response.headers:
+            total_pages = int(response.headers["X-WP-TotalPages"])
+            if page >= total_pages:
+                break
+
         page += 1
 
-    return products
+    return all_products
 
 
 # -------------------------------------------------------
